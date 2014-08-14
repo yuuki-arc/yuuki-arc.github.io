81025c5c5f3117b0f5eb51d7aafbb27090cd482d
yuuki-arc <arcs7nine@gmail.com>
2014-08-14 13:52:07 +0900

    Asset Pipeline回りを整理

--- config.rb
+++ config.rb
@@ -1,4 +1,37 @@
 ###
+# Blog settings
+###
+
+Time.zone = "Tokyo"
+
+activate :blog do |blog|
+  # This will add a prefix to all links, template references and source paths
+  blog.prefix = "blog"
+
+  # blog.permalink = "{year}/{month}/{day}/{title}.html"
+  # Matcher for blog source files
+  # blog.sources = "{year}-{month}-{day}-{title}.html"
+  # blog.taglink = "tags/{tag}.html"
+  # blog.layout = "layout"
+  # blog.summary_separator = /(READMORE)/
+  # blog.summary_length = 250
+  # blog.year_link = "{year}.html"
+  # blog.month_link = "{year}/{month}.html"
+  # blog.day_link = "{year}/{month}/{day}.html"
+  blog.default_extension = ".md"
+
+  # blog.tag_template = "tag.html"
+  # blog.calendar_template = "calendar.html"
+
+  # Enable pagination
+  blog.paginate = true
+  blog.per_page = 5
+  blog.page_link = "page/{num}"
+end
+
+page "/feed.xml", layout: false
+
+###
 # Compass
 ###
 
@@ -14,10 +47,10 @@
 # Per-page layout changes:
 #
 # With no layout
-# page "/path/to/file.html", :layout => false
+# page "/path/to/file.html", layout: false
 #
 # With alternative layout
-# page "/path/to/file.html", :layout => :otherlayout
+# page "/path/to/file.html", layout: :otherlayout
 #
 # A path which all have the same layout
 # with_layout :admin do
@@ -25,8 +58,8 @@
 # end
 
 # Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
-#  :which_fake_page => "Rendering a fake page with a local variable" }
+# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
+#  which_fake_page: "Rendering a fake page with a local variable" }
 
 ###
 # Helpers
@@ -36,9 +69,7 @@
 # activate :automatic_image_sizes
 
 # Reload the browser automatically whenever files change
-# configure :development do
-#   activate :livereload
-# end
+activate :livereload
 
 # Methods defined in the helpers block are available in templates
 # helpers do
@@ -51,7 +82,12 @@ set :css_dir, 'css'
 
 set :js_dir, 'js'
 
-set :images_dir, 'img'
+set :images_dir, 'images'
+
+after_configuration do
+  sprockets.append_path "#{root}/source/css-theme"
+  sprockets.append_path "#{root}/source/js-theme"
+end
 
 # Build-specific configuration
 configure :build do
@@ -71,29 +107,6 @@ configure :build do
   # set :http_prefix, "/Content/images/"
 end
 
-# デプロイ設定
-activate :deploy do |deploy|
-  deploy.method = :git
-  deploy.branch = 'master'
-end
-
-# ブログ基本設定
-Time.zone = "Tokyo"
-
-activate :blog do |blog|
-  blog.prefix = "blog"
-  # blog.taglink = "tags/:tag.html"
-  # blog.layout = "layouts/layout"
-  blog.default_extension = ".md"
-
-  # blog.tag_template = "tag.html"
-  # blog.calendar_template = "calendar.html"
-
-  blog.paginate = true
-  blog.per_page = 5
-  blog.page_link = "page/:num"
-end
-
 # markdown
 set :markdown_engine, :redcarpet
 #set :markdown, :fenced_code_blocks => true, :smartypants => true
@@ -104,6 +117,8 @@ activate :syntax, :line_numbers => true
 activate :rouge_syntax, :lineanchor => 'line'
 activate :directory_indexes
 
-# Atom
-page "/feed.xml", :layout => false
-activate :livereload
+# デプロイ設定
+activate :deploy do |deploy|
+  deploy.method = :git
+  deploy.branch = 'master'
+end
